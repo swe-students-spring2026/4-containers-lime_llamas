40e250476c7df16690f0295d9208428b153033dd
lauraliu518 <yixuanliu0518@gmail.com>
2026-04-13 12:22:02 -0400

    fix: pass pylint + add missing deps

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,14 @@
+"""Main Flask application for Bird Detection Dashboard."""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
+    """Render the homepage."""
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)

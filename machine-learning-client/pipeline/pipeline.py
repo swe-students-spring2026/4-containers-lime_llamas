7b9eac91bc9e46b450006b5b0d81886b37a13fb5
hewlett-packard-lovecraft <hx2314@nyu.edu>
2026-04-14 17:12:44 -0400

    tomli

--- machine-learning-client/pipeline/pipeline.py
+++ machine-learning-client/pipeline/pipeline.py
@@ -1,5 +1,7 @@
+"""birdnet pipeline"""
 
 
-# there should only be one of these per model per process otherwise we load the weights multiple times
+# there should only be one of these per model per process otherwise we load the
+# weights multiple times
 
 

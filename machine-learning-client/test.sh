1e6a3dd55d21c56efaa71af0ec7935c3ac269c27
hewlett-packard-lovecraft <hx2314@nyu.edu>
2026-04-15 18:46:05 -0400

    add test scripts, fix compose file

--- machine-learning-client/test.sh
+++ machine-learning-client/test.sh
@@ -1,20 +1,18 @@
-
 #!/usr/bin/bash
 
 pushd example
 
 ffmpeg -i Colaptes_auratus_XC104537.ogg -f segment -segment_time 3 -c copy Colaptes_auratus_XC104537_%03d.ogg
 
-
 for i in {001..008}; do
-    file="Colaptes_auratus_XC104537_$i.ogg"
+	file="Colaptes_auratus_XC104537_$i.ogg"
 
-    echo file
+	echo file
 
-    curl -X POST \
-        -H "Content-Type: audio/ogg" \
-        --data-binary "@${file}"  \
-        http://localhost:8000/analyze | jq
+	curl -X POST \
+		-H "Content-Type: audio/ogg" \
+		--data-binary "@${file}" \
+		http://localhost:8000/analyze | python3 -m json.tool
 done
 
 popd
